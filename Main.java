0afcefe39a84783e80f07eeb38d67b81113b2cf7
burcj3622 <burcj3622@wrdsb.ca>
2020-10-05 19:27:58 +0000

    animals

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -10,6 +11,36 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+
+  
+  
+// does the animal have feathers
+System.out.println("Does your animal have feathers?");
+String feathers = input.nextLine();
+ if( feathers.equals("yes") ) {
+   // animal has feathers
+   System.out.println("Does the animal swim?");
+   String swim = input.nextLine();
+   if(swim.equals("yes")){
+  System.out.println("This is a duck");
+   }else if (swim.equals("no")){
+     System.out.println("This is a hen");
+   }
+   
+   }else{
+    if( feathers.equals("no") ) {
+   System.out.println("Does the animal legs?");
+   String legs = input.nextLine();
+   if(legs.equals("yes")){
+   System.out.println("This is a lizard");
+   }else if (legs.equals("no")){
+    System.out.println("This is a snake");
+     
+
     
+   }
+  }
+}
   }
 }
